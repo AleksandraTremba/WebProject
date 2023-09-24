cc32faec6b9b282df3525c93bc284b608c390129
Maksim Usmanov <mausma@taltech.ee>
2023-09-24 15:19:08 +0300

    Security is configured the VERY BAREBONES WAY, still no authentication for requests excluding /api/users/**

--- src/main/java/ee/taltech/iti0302/okapi/backend/controllers/CustomerController.java
+++ src/main/java/ee/taltech/iti0302/okapi/backend/controllers/CustomerController.java
@@ -1,10 +1,11 @@
 package ee.taltech.iti0302.okapi.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ee.taltech.iti0302.okapi.backend.dto.CustomerDTO;
@@ -23,13 +24,13 @@ public class CustomerController {
     }
 
     @PostMapping("login")
-    public boolean login(@RequestParam CustomerDTO customer) {
-        boolean exists = userRepository.existsByUsername(customer.getUsername());
-        return exists;
+    public boolean login(@RequestBody CustomerDTO customer) {
+        return userRepository.existsByUsername(customer.getUsername());
     }
 
     @PostMapping("register")
-    public Customer registerCustomer(@RequestBody CustomerDTO customer) {
-        return userRepository.save(new Customer(customer.getUsername(), customer.getPassword()));
+    @ResponseStatus(HttpStatus.OK)
+    public void registerCustomer(@RequestBody CustomerDTO customer) {
+        userRepository.save(new Customer(customer.getUsername(), customer.getPassword()));
     }
 }
