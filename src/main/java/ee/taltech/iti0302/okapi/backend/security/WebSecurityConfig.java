9b1dae72a238c045149d9cc46ae2caf471666d85
Maksim Usmanov <mausma@taltech.ee>
2023-10-22 21:25:38 +0300

    Migrated customer services to Lombok

--- src/main/java/ee/taltech/iti0302/okapi/backend/security/WebSecurityConfig.java
+++ src/main/java/ee/taltech/iti0302/okapi/backend/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -19,7 +20,7 @@ public class WebSecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/api/users/**")).permitAll()
                         .anyRequest().permitAll())
             .httpBasic(it -> {})
-            .csrf((csrf) -> csrf.disable());
+            .csrf(AbstractHttpConfigurer::disable);
             // .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
 
         return http.build();
--- src/main/java/ee/taltech/iti0302/okapi/backend/services/CustomerService.java
+++ src/main/java/ee/taltech/iti0302/okapi/backend/services/CustomerService.java
@@ -1,15 +1,16 @@
 package ee.taltech.iti0302.okapi.backend.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ee.taltech.iti0302.okapi.backend.entities.Customer;
 import ee.taltech.iti0302.okapi.backend.repository.CustomerRepository;
 
+@RequiredArgsConstructor
 @Service
 public class CustomerService {
-    @Autowired
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
     public boolean customerExists(Customer customer) {
         return customerRepository.existsByUsername(customer.getUsername());
