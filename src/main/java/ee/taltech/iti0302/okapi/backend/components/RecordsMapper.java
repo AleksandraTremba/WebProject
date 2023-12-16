0258442fa017632a8b4436e494f9475422a44b06
aleks <altrem@ttu.ee>
2023-12-17 01:27:08 +0200

    timer and records services tests

--- src/main/java/ee/taltech/iti0302/okapi/backend/components/RecordsMapper.java
+++ src/main/java/ee/taltech/iti0302/okapi/backend/components/RecordsMapper.java
@@ -3,6 +3,7 @@ package ee.taltech.iti0302.okapi.backend.components;
 import ee.taltech.iti0302.okapi.backend.dto.records.RecordsDTO;
 import ee.taltech.iti0302.okapi.backend.entities.Records;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,9 @@ public interface RecordsMapper {
 
     RecordsMapper INSTANCE = Mappers.getMapper(RecordsMapper.class);
 
+    @Mapping(source = "numberOfCustomers", target = "numberOfCustomers")
     RecordsDTO toDTO(Records records);
+
+    @Mapping(source = "numberOfCustomers", target = "numberOfCustomers")
     Records toEntity(RecordsDTO dto);
 }
