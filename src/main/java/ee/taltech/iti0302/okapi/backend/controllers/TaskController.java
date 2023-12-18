1d67ff8fab86f4c52f437121da586a2ae2663d0d
Maksim Usmanov <mausma@taltech.ee>
2023-12-18 17:44:36 +0200

    final touches

--- src/main/java/ee/taltech/iti0302/okapi/backend/controllers/TaskController.java
+++ src/main/java/ee/taltech/iti0302/okapi/backend/controllers/TaskController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/tasks")
-@CrossOrigin(origins = "http://127.0.0.1:8081")
 public class TaskController {
     private final TaskService taskService;
 
